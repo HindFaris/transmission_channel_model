a9cee989c30960f3b433fb1190b88d0fb96454fe
jerom <jerom@10.29.236.75>
2022-09-14 11:07:37 +0200

    Signal NRZ avancé

--- SIT213_Project/src/signaux/Signal.java
+++ SIT213_Project/src/signaux/Signal.java
@@ -7,15 +7,21 @@ public abstract class Signal<R,E> {
 	
 	protected Information<E> signalEntree;
 	
-	protected Information<R> signalsorieInformation;
+	protected Information<R> signalSortieInformation;
+	
+	protected int nbEchantillon;
 	
 	public Signal() {
 		signalEntree = new Information<E>();
 	}
 	
-	public Signal(Information<E> informationRecue) {
+	public Signal(Information<E> informationRecue, int nbEchantillons) {
 		signalEntree = informationRecue;
 	}
 	
-	public abstract Information<R> generer();
+	public abstract void generer();
+	
+	public Information<R> signalSortieInformation(){
+		return signalSortieInformation;
+	}
 }
--- SIT213_Project/src/signaux/SignalRZ.java
+++ SIT213_Project/src/signaux/SignalRZ.java
@@ -5,7 +5,7 @@ import information.Information;
 public class SignalRZ<R, E> extends Signal<R, E>{
 
 	
-	public Information<R> generer(){
+	public void generer(){
 		
 	}
 }
