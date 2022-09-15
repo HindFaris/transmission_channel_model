2cef425342d6498294fd6eb82b510b65846daf85
aina2 <aina2@PC-ainadirou>
2022-09-15 18:31:09 +0200

    sourceAleatoireTest not done

---
+++ SIT213_Project/src/tests/SourceAleatoireTest.java
@@ -0,0 +1,16 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+public class SourceAleatoireTest {
+	
+	//TODO test : verifier que si seed = 0 -> valeur par defaut (100)
+	private int nBitMess = 23;
+	private int seed = 10;
+	
+	@Test
+	public SourceAleatoireTest(int nBitMess, int seed) {
+		
+	}
+}
