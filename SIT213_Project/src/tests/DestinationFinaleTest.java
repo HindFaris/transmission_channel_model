74b9f0192c3cec826d9a945a199349ba2431452a
aina2 <aina2@PC-ainadirou>
2022-09-15 19:05:48 +0200

    destination finale test

---
+++ SIT213_Project/src/tests/DestinationFinaleTest.java
@@ -0,0 +1,17 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import destinations.Destination;
+
+
+public class DestinationFinaleTest {
+	
+	 private String Binary = new String("0111000111");
+	
+	//@Test
+	 public DestinationFinaleTest (String Binary) {
+		Destination DestinationF = new Destination();
+		//assertEquals(DestinationF.getInformationRecue,);
+	}
+}
