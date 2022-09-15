645ed93516a96afd0badb79ee55bd50fcd2bd110
aina2 <aina2@PC-ainadirou>
2022-09-15 19:00:30 +0200

    test : emetteur recepteur sourceAleatoire

--- SIT213_Project/src/tests/SourceAleatoireTest.java
+++ SIT213_Project/src/tests/SourceAleatoireTest.java
@@ -2,15 +2,18 @@ package tests;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import sources.SourceAleatoire;
+
 
 public class SourceAleatoireTest {
 	
-	//TODO test : verifier que si seed = 0 -> valeur par defaut (100)
 	private int nBitMess = 23;
-	private int seed = 10;
+	private int seed = 10; //TODO test seed = 0
 	
-	@Test
+	//@Test
 	public SourceAleatoireTest(int nBitMess, int seed) {
-		
+		SourceAleatoire SourceA = new SourceAleatoire(nBitMess, seed);
+		//assertEquals(SourceA.getInformationGeneree, , "L'information generee ne correspond pas");
+		//asserEquals(SourceA.getSeed, int 10, "La valeur de seed ne correspond pas");
 	}
 }
