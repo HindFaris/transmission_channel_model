c5d872948cf25984b373ac9e177c4930712324ca
gaelc <gaelc@DESKTOP-55PPDB8>
2022-09-15 18:18:32 +0200

    1er push test

---
+++ SIT213_Project/src/tests/SourceFixeTest.java
@@ -0,0 +1,16 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import sources.*;
+
+public class SourceFixeTest {
+	
+	 private String Binary = new String("0111000111");
+	
+	//@Test
+	 public SourceFixeTest (String Binary) {
+		SourceFixe SourceF = new SourceFixe(Binary);
+		//assertEquals(SourceF.getInformationGeneree,);
+	}
+}
