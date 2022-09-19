5de57e0be8eadccfa608a34ad336ba3851fe9d55
aina2 <aina2@PC-ainadirou>
2022-09-19 16:31:01 +0200

    tests source fixe

---
+++ SIT213_Project/src/tests/InvalideParameterException.java
@@ -0,0 +1,5 @@
+package tests;
+
+public class InvalideParameterException extends Exception {
+
+}
--- SIT213_Project/src/tests/SourceFixeTest.java
+++ SIT213_Project/src/tests/SourceFixeTest.java
@@ -26,6 +26,16 @@ public class SourceFixeTest {
 	//@Test
 	 public SourceFixeTest (String Binary) {
 		SourceFixe SourceF = new SourceFixe(Binary);
-		//assertEquals(SourceF.getInformationGeneree,binMess, "Linformation generee ne correspond pas au message entre");
+		assertEquals(SourceF.getInformationGeneree(),Binary, "L'information generee ne correspond pas au message entre");
 	}
+	 
+	 public boolean SourceFixeLengthTest (String Binary) throws InvalideParameterException {
+		 SourceFixe Source = new SourceFixe(Binary);
+		 if (Source.getInformationGeneree().nbElements()<7) {
+			 //comment tester que ca devient une source aleatoire
+			 //comment tester que la source fixe n'est pas creee
+			 return true;
+		 }else return false;	 
+	 }
+
 }
