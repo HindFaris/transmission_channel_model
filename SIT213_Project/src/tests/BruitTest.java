3dd5de2adf262ba74588852c4998cb9018532b0e
jerom <jerom@10.29.238.253>
2022-09-23 00:17:30 +0200

    d

--- SIT213_Project/src/tests/BruitTest.java
+++ SIT213_Project/src/tests/BruitTest.java
@@ -1,7 +1,11 @@
 package tests;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.LinkedList;
+
 import org.junit.jupiter.api.Test;
 
+import information.Information;
 import signaux.Bruit;
 
 
@@ -23,21 +27,46 @@ public class BruitTest {
 		assertEquals(Bruit.getNbEchantillons(), nbEchantillons, "Le nombre d'echantillon ne correspond pas");
 		nbErrors--;
 	}
-	
+
+	 public LinkedList<LinkedList <Float>> autoCorrelateTest() 
+	 {
+		 Bruit br = new Bruit(4, 8, 50);
+		 LinkedList<Float> sum = new LinkedList <Float> (); 
+		 LinkedList<LinkedList <Float>> output = new LinkedList<LinkedList <Float>> ();
+		 float a = 0;
+
+	     for (int n = 0; n < br.getSignalSortieInformation().nbElements(); n++) 
+	     {
+	         for (int i = n; i < br.getSignalSortieInformation().nbElements(); i++) 
+	         {
+	        	a = (float) br.getSignalSortieInformation().iemeElement(i) * (float) br.getSignalSortieInformation().iemeElement(i-n);
+				sum.add(a);
+
+	         }
+	         output.add(sum);
+	         System.out.println(output);
+
+	         sum.clear();
+	     }
+	     return output;
+
+	 }
+
 	@Test
 	public static Tests testReport() {
 		Tests tr;
 		
 		float ecartType = 5;
-		int tailleSignalEntree = 20;
+		int tailleSignalEntree = 200000;
 		int nbEchantillons = 10000;
 		
 		BruitTest E = new BruitTest();
-		
+
 		nbTests+=3;
 		E.BruitInitTest(ecartType, tailleSignalEntree, nbEchantillons);
-		
-		
+
+		E.autoCorrelateTest();
+
 		tr = new Tests(nbTests,nbErrors);
 		return tr; 
 	}
