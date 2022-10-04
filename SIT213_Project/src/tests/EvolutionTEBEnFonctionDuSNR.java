869c772cbb4497592d2df104261717bc6580139a
jerom <jerom@314159265358979>
2022-10-04 23:31:29 +0200

    Révision de la complexité

--- SIT213_Project/src/tests/EvolutionTEBEnFonctionDuSNR.java
+++ SIT213_Project/src/tests/EvolutionTEBEnFonctionDuSNR.java
@@ -14,18 +14,21 @@ public class EvolutionTEBEnFonctionDuSNR {
 		PrintStream myConsole = new PrintStream(new File(CHEMIN));
 		System.setOut(myConsole);
 		
-		System.out.println("Simulations pour un nombre d'echantillons de 10000");
-		System.out.println("SNR par bit (dB);TEB signal NRZ;TEB signal NRZT;TEB signal RZ");
+		System.out.println("Simulations pour un nombre d'echantillons de 1000");
+		System.out.println("SNR par bit (dB);TEB signal NRZ bruite;TEB signal NRZ bruite avec codage;TEB signal NRZ multi trajets; TEB signal NRZ multi trajets avec codage");
 		for(float indice = -50; indice<=20; indice += 0.5) {
-			Simulateur simulateurNRZ =  new Simulateur(new String[] {"-mess","10000", "-form", "NRZ", "-snrpb", String.valueOf(indice)});
-			simulateurNRZ.execute();
-			Simulateur simulateurNRZT =  new Simulateur(new String[] {"-mess","10000", "-form", "NRZT", "-snrpb", String.valueOf(indice)});
-			simulateurNRZT.execute();
-			Simulateur simulateurRZ =  new Simulateur(new String[] {"-mess","10000", "-form", "RZ", "-snrpb", String.valueOf(indice)});
-			simulateurRZ.execute();
-			System.out.println(Float.toString(indice) + ";" + Float.toString(simulateurNRZ.calculTauxErreurBinaire()) + ";" + 
-					Float.toString(simulateurNRZT.calculTauxErreurBinaire()) + ";" + Float.toString(simulateurRZ.calculTauxErreurBinaire()));
-		}//
+			Simulateur simulateur1 =  new Simulateur(new String[] {"-mess","10000", "-form", "NRZ", "-snrpb", String.valueOf(indice)});
+			simulateur1.execute();
+			Simulateur simulateur2 =  new Simulateur(new String[] {"-mess","10000", "-form", "NRZ", "-snrpb", String.valueOf(indice), "-codeur"});
+			simulateur2.execute();
+			Simulateur simulateur3 =  new Simulateur(new String[] {"-mess","10000", "-form", "NRZ", "-snrpb", String.valueOf(indice), "-ti", "15", "0.7", "25", "0.4"});
+			simulateur3.execute();
+			Simulateur simulateur4 =  new Simulateur(new String[] {"-mess","10000", "-form", "NRZ", "-snrpb", String.valueOf(indice), "-ti", "15", "0.7", "25", "0.4", "-codeur"});
+			simulateur4.execute();
+			System.out.println(Float.toString(indice) + ";" + Float.toString(simulateur1.calculTauxErreurBinaire()) + ";" + 
+					Float.toString(simulateur2.calculTauxErreurBinaire()) + ";" + Float.toString(simulateur3.calculTauxErreurBinaire())
+					+ ";" + Float.toString(simulateur4.calculTauxErreurBinaire()));
+		}
 		myConsole.close();
 	}
 }
--- SIT213_Project/src/transmetteurs/TransmetteurAnalogiqueMultiTrajets.java
+++ SIT213_Project/src/transmetteurs/TransmetteurAnalogiqueMultiTrajets.java
@@ -1,6 +1,9 @@
 package transmetteurs;
 
 import java.util.LinkedList;
+
+import javax.management.ConstructorParameters;
+
 import destinations.DestinationInterface;
 import information.*;
 import signaux.Bruit;
@@ -92,8 +95,7 @@ public class TransmetteurAnalogiqueMultiTrajets extends Transmetteur<Float, Floa
 				float var = informationAjouteeCopie.get(0)+information.iemeElement(0);
 				information.remove(0);
 				informationAjouteeCopie.remove(0);
-				//informationAjoutee.add(var);
-				informationAjoutee.setIemeElement(i, var);
+				informationAjoutee.add(var);
 			} 
 
 
