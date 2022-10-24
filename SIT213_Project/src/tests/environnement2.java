f3a3caf5d0fe8eb8001403379ae71080dca02013
jerom <jerom@10.29.238.75>
2022-10-24 21:04:12 +0200

    environnement 2

--- SIT213_Project/src/tests/environnement2.java
+++ SIT213_Project/src/tests/environnement2.java
@@ -18,7 +18,7 @@ public class environnement2 {
 	public environnement2() {
 		// TODO Auto-generated constructor stub
 	}
-	private final static String CHEMIN = "C:\\Professionnel\\IMT Atlantique\\Info\\Java\\projet2A\\courbes\\environnement2.txt";	//Please respect this format
+	private final static String CHEMIN = "C:\\Professionnel\\IMT Atlantique\\Info\\Java\\projet2A\\courbes\\environnement2V2.txt";	//Please respect this format
 
 	/**
 	 * Methode qui genere les tests pour l'environnement
@@ -29,23 +29,25 @@ public class environnement2 {
 	public static void main(String[] args) throws ArgumentsException, Exception{
 		PrintStream myConsole = new PrintStream(new File(CHEMIN));
 		System.setOut(myConsole);
-
-		System.out.println("Simulations pour un nombre d'echantillons de 999999 avec seed");
-		System.out.println("SNR par bit (dB);NRZ Te=1*10e-6s;NRZ Te=2*10e-6s;RZ Te=1*10e-6s;RZ Te=2*10e-6s");
-		for(float indice = 0; indice<=25; indice += 1) {
-			Simulateur simulateurNRZ1 =  new Simulateur(new String[] {"-mess","999999", "-form", "NRZ", "-snrpb", String.valueOf(indice), "-ti", "10", "0.5", "-nbEch", "1","-seed","1"});
-			simulateurNRZ1.execute();
-			Simulateur simulateurNRZ2 =  new Simulateur(new String[] {"-mess","999999", "-form", "NRZ", "-snrpb", String.valueOf(indice), "-ti", "10", "0.5", "-nbEch", "2"});
-			simulateurNRZ2.execute();
-			Simulateur simulateurRZ1 =  new Simulateur(new String[] {"-mess","999999", "-form", "RZ", "-snrpb", String.valueOf(indice), "-ti", "10", "0.5", "-nbEch", "1"});
-			simulateurRZ1.execute();
-			Simulateur simulateurRZ2 =  new Simulateur(new String[] {"-mess","999999", "-form", "RZ", "-snrpb", String.valueOf(indice), "-ti", "10", "0.5", "-nbEch", "2"});
-			simulateurRZ2.execute();
+		String aAfficher;
+		
+		System.out.println("Simulations pour un nombre d'echantillons de 100000 avec seed");
+		System.out.println("SNR par bit (dB);NRZ avec 1 echantillon;RZ avec 1 echantillon;NRZ avec 11 echantillons;RZ avec 11 echantillons;"
+				+ "NRZ avec 21 echantillons;RZ avec 21 echantillons;NRZ avec 31 echantillons;RZ avec 31 echantillons;"
+				+ "NRZ avec 41 echantillons;RZ avec 41 echantillons;");
+		for(float snrpb = 0; snrpb<=25; snrpb += 1) {
 			
-			System.out.println(Float.toString(indice) + ";" + Float.toString(simulateurNRZ1.calculTauxErreurBinaire()) + ";" + 
-					Float.toString(simulateurNRZ2.calculTauxErreurBinaire()) + ";" + Float.toString(simulateurRZ1.calculTauxErreurBinaire()) +
-					";" + Float.toString(simulateurRZ2.calculTauxErreurBinaire()));
+			aAfficher=Float.toString(snrpb);
+			for(int nbEch  = 1; nbEch<42; nbEch+=10) {
+				Simulateur simulateurNRZ =  new Simulateur(new String[] {"-mess","100000", "-form", "NRZ", "-snrpb", String.valueOf(snrpb), "-ti", "10", "0.5", "-nbEch", String.valueOf(nbEch),"-seed","1"});
+				simulateurNRZ.execute();
+
+				Simulateur simulateurRZ =  new Simulateur(new String[] {"-mess","100000", "-form", "RZ", "-snrpb", String.valueOf(snrpb), "-ti", "10", "0.5", "-nbEch", String.valueOf(nbEch), "-seed", "1"});
+				simulateurRZ.execute();
+				aAfficher+=";"+Float.toString(simulateurNRZ.calculTauxErreurBinaire())+";"+Float.toString(simulateurRZ.calculTauxErreurBinaire());
+			}
+			System.out.println(aAfficher);
 		}
-		myConsole.close();
+		
 	}
 }
